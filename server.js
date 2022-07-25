d42c3834e3889bd54fe9ed2f3c176b7062cb1d0e
Kilimba <dyaul003@umn.edu>
2022-07-24 23:06:08 -0500

    got one of my add functions to work

--- server.js
+++ server.js
@@ -77,29 +77,80 @@ const userPrompt = () => {
 // View all employees
 const viewEmployees = () => {
   db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee.roles_id,
-  CONCAT(employee.first_name, ' ', employee.last_name) AS manager
+  CONCAT(manager.first_name, ' ', manager.last_name) AS manager
   FROM employee manager
-  JOIN employee ON employee.manager_id = manager.id; `, 
+  RIGHT JOIN employee ON employee.manager_id = manager.id; `, 
   (err, rows) => {
     console.table(rows);
+    return userPrompt();
     });
 };
 
 // Add Employee
 const addEmployee = () => {
-  db.query(`jj` ,
+  return inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is your first name?',
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is your last name?',
+  },
+  {
+    type: 'input',
+    name: 'roles_id',
+    message: 'What is your role?',
+},
+{
+  type: 'input',
+  name: 'manager_id',
+  message: 'Who is your manager? (id)',
+},
+])
+.then(val => {
+  let params = [
+    val.first_name,
+    val.last_name,
+    val.roles_id,
+    val.manager_id
+  ]
+
+let sql = `INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES (?,?,?,?)`;
+  db.query(sql, params, (err, rows) => {
+    console.table(rows);
+    return userPrompt();
+
+  });
+});
+};
+
+
+
+
+
+
+// Updated employee role
+const updateRole = () => {
+  db.query(`` ,
   (err, rows) => {
     console.table(rows);
     });
 };
 
-// Updated employee role
-const updateRole = () => {
-  db.query(`jj` ,
-  (err, rows) => {
-    console.table(rows);
-    });
-};
+// const sql = `UPDATE candidates SET party_id = ? 
+//                WHERE id = ?`;
+//   const params = [req.body.party_id, req.params.id];
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//     }
+//   });
+
 
 // View all roles
 const viewRoles = () => {
@@ -108,6 +159,7 @@ const viewRoles = () => {
   LEFT JOIN department ON roles.department_id = department.id; `, 
   (err, rows) => {
     console.table(rows);
+    return userPrompt();
     });
 };
 
@@ -116,6 +168,7 @@ const addRoles = () => {
   db.query(`jj` ,
   (err, rows) => {
     console.table(rows);
+    return userPrompt();
     });
 };
 
@@ -125,6 +178,7 @@ const viewDepartments = () => {
   FROM department; `, 
   (err, rows) => {
     console.table(rows);
+    return userPrompt();
     });
 };
 
@@ -133,14 +187,16 @@ const addDepartment = () => {
   db.query(`jj` ,
   (err, rows) => {
     console.table(rows);
+    return userPrompt();
     });
 };
 
-// Add department
+// Update department
 const updateDepartment = () => {
   db.query(`jj` ,
   (err, rows) => {
     console.table(rows);
+    return userPrompt();
     });
 };
 
